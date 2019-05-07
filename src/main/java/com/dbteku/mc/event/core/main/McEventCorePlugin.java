19d59efb481e80a500c10c1ee52d280f5fd05ac9
dbteku <dbteku@hotmail.com>
2019-05-07 17:09:00 -0600

    Init to plugin

---
+++ src/main/java/com/dbteku/mc/event/core/main/McEventCorePlugin.java
@@ -0,0 +1,29 @@
+package com.dbteku.mc.event.core.main;
+
+import org.bukkit.plugin.ServicePriority;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import com.dbteku.javaevents.EventManager;
+
+
+public class McEventCorePlugin extends JavaPlugin{
+
+	private static final String MAIN_ERROR = "You cannot run this file its a library!";
+	
+	@Override
+	public void onDisable(){
+		System.out.println("MCEventCore disabled.");
+	}
+	
+	@Override
+	public void onEnable(){
+		getServer().getConsoleSender().sendMessage("McEventCore Enabled!");
+		// Initialize Event
+		getServer().getServicesManager().register(EventManager.class, EventManager.getInstance(), this, ServicePriority.Highest);
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(MAIN_ERROR);
+	}
+	
+}
